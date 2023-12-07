62439dc5226e9956642390ee457705bf0b57f4cf
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-12-07 17:42:15 +0100

    Changed the function name to a proper one

--- lib/day_01/day_01.ex
+++ lib/day_01/day_01.ex
@@ -5,14 +5,14 @@ defmodule Day1 do
   def solution() do
     File.read!("lib/day_01/data.txt")
     |> String.split("\n", trim: true)
-    |> Enum.map(fn line -> filter_digits_and_newline(line) end)
+    |> Enum.map(fn line -> filter_digits(line) end)
     |> Enum.map(fn digits -> <<List.first(digits)>> <> <<List.last(digits)>> end)
     |> Enum.map(fn digits -> {n, _} = Integer.parse(digits, 10); n end)
     |> Enum.sum()
     |> IO.inspect()
   end
 
-  defp filter_digits_and_newline(line) do
+  defp filter_digits(line) do
     line
     |> String.to_charlist()
     |> Enum.filter(fn char -> Guards.is_digit(char) end)
